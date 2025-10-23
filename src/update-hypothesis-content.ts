9f88bc064f95a812103029422791e075302b4537
Scott Aubrey <scott@aubrey.org.uk>
2025-10-23 14:28:38 +0100

    Add script for updating Hypothesis content

---
+++ src/update-hypothesis-content.ts
@@ -0,0 +1,32 @@
+import data from '../data/imgur-image-urls-with-manuscript-id-and-cdn-url-and-published-url-and-imgur-link-url.json';
+
+data.splice(0, 1).forEach(async ({ hypothesis_id, image_info }) => {
+  const annotationResponse = await fetch(`https://api.hypothes.is/api/annotations/${hypothesis_id}`);
+  if (!annotationResponse.ok) {
+    throw new Error(`Failed to fetch annotation for hypothesis_id ${hypothesis_id}: ${annotationResponse.status} ${annotationResponse.statusText}`);
+  }
+
+  const annotation = await annotationResponse.json();
+  let content = annotation.text;
+  content = content.replaceAll(' title="source: imgur.com"', '');
+  image_info.forEach(({ imgur_link_url, imgur_url, published_url }) => {
+    content = content.replaceAll(imgur_link_url, published_url);
+    content = content.replaceAll(imgur_url, published_url);
+  });
+  console.log(content);
+  // const updateResponse = await fetch(`https://api.hypothes.is/api/annotations/${hypothesis_id}`, {
+  //   method: 'PUT',
+  //   headers: {
+  //     'Authorization': `Bearer ${process.env.HYPOTHESIS_API_TOKEN}`,
+  //     'Content-Type': 'application/json'
+  //   },
+  //   body: JSON.stringify({
+  //     ...annotation,
+  //     content: contentWithImages
+  //   })
+  // });
+
+  // if (!updateResponse.ok) {
+  //   throw new Error(`Failed to update annotation for hypothesis_id ${hypothesis_id}: ${updateResponse.status} ${updateResponse.statusText}`);
+  // }
+});
